76ca76cae41dd5bfef94c4acf18ecb0df45c72d8
Fidi W P <feedeewepe@gmail.com>
2021-06-28 07:15:51 +0700

    siap

---
+++ app/Controllers/BaseController.php
@@ -0,0 +1,73 @@
+<?php
+namespace App\Controllers;
+
+/**
+ * Class BaseController
+ *
+ * BaseController provides a convenient place for loading components
+ * and performing functions that are needed by all your controllers.
+ * Extend this class in any new controllers:
+ *     class Home extends BaseController
+ *
+ * For security be sure to declare any new methods as protected or private.
+ *
+ * @package CodeIgniter
+ */
+
+use CodeIgniter\Controller;
+use CodeIgniter\HTTP\IncomingRequest;
+use CodeIgniter\Config\Services;
+use Config\Database;
+
+class BaseController extends Controller
+{
+	/**
+     * @var \Myth\Auth\Authorization\FlatAuthorization
+     */
+    protected $authorize;
+
+    /**
+     * @var \Myth\Auth\Authentication\LocalAuthenticator
+     */
+    protected $auth;
+
+    /**
+     * @var \CodeIgniter\Database\BaseConnection|\CodeIgniter\Database\BaseBuilder
+     */
+    protected $db;
+
+	/**
+	 * GetPost, GetVar dll
+	 *
+	 * @var IncomingRequest;
+	 */
+	protected $request;
+
+	/**
+	 * An array of helpers to be loaded automatically upon
+	 * class instantiation. These helpers will be available
+	 * to all other controllers that extend BaseController.
+	 *
+	 * @var array
+	 */
+	protected $helpers = ['auth', 'url', 'form','html','text'];
+
+	/**
+	 * Constructor.
+	 */
+	public function initController(\CodeIgniter\HTTP\RequestInterface $request, \CodeIgniter\HTTP\ResponseInterface $response, \Psr\Log\LoggerInterface $logger)
+	{
+		// Do Not Edit This Line
+		parent::initController($request, $response, $logger);
+
+		//--------------------------------------------------------------------
+		// Preload any models, libraries, etc, here.
+		//--------------------------------------------------------------------
+		// E.g.:
+		// $this->session = \Config\Services::session();
+		$this->auth = Services::authentication();
+        $this->authorize = Services::authorization();
+        $this->db = Database::connect();
+	}
+
+}
